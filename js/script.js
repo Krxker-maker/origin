d4afdc0ffd0d9da92743a6b4f90bab912b7242a9
Artur <example@gmail.com>
2024-09-13 16:37:42 +0300

    Добавление switch

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 "use strict";
-const numberofFilms = +prompt('Сколько фильмов вы посмотрели?',"");
-const personalMovieDB = {
-count: numberofFilms,
-movies: {},
-actors: {},
-genres:[],
-privat:false,
-};
+// const numberofFilms = +prompt('Сколько фильмов вы посмотрели?',"");
+// const personalMovieDB = {
+// count: numberofFilms,
+// movies: {},
+// actors: {},
+// genres:[],
+// privat:false,
+// };
 
-const a = prompt('Один из последних просмотреных фильмов?',''),
-      b =  prompt('Насколько оцените его?',''),
-      c = prompt('Один из последних просмотреных фильмов?',''),
-      d = prompt('Насколько оцените его?','');
+// const a = prompt('Один из последних просмотреных фильмов?',''),
+//       b =  prompt('Насколько оцените его?',''),
+//       c = prompt('Один из последних просмотреных фильмов?',''),
+//       d = prompt('Насколько оцените его?','');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-      console.log(personalMovieDB);
+//       personalMovieDB.movies[a] = b;
+//       personalMovieDB.movies[c] = d;
+    //   console.log(personalMovieDB);
+    
+    //   if(num<49){
+    //     console.log('Error');
+    //   }else if(num>100){
+    //     console.log('A lot of');
+    //   }else{
+    //     console.log('Right!');
+    //   }
+    const num = 50;
+switch(num){
+case 49:
+    console.log('Неверно');
+    break;
+    case 100:
+        console.log('Неверно');
+        break;
+        case 51:
+            console.log('А ты хорош)');
+            break;
+            default:
+                console.log('Не в этот раз :(');
+                break;
+}
