05165385665ca8cc7b26ac6fabf6f6cbd836be69
Artur <example@gmail.com>
2024-09-13 14:18:21 +0300

    firt commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+let number = 20;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+const storeName ='gucci';
+const storeDescription = {
+    budget:10000,
+    employes:[
+        'dima','anna','katya'
+        ],
+         products:{
+            potatoe:20,
+            sprite:10
+             },
+               open:true
+}
+console.log(storeDescription);
+
+
+// const answers = []
+// answers[0] = prompt('Write your name?', '');
+// answers[1] = prompt('Write your surename?', '');
+// answers[2] = prompt('Write your age?', '');
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+    
