9bfd4944d0eef08fe801ae2be72432e2848b2f3b
Artur <example@gmail.com>
2024-09-13 15:51:04 +0300

    Добавлено новое изменение в коде для управления фильмами

--- js/script.js
+++ js/script.js
@@ -1,36 +1,18 @@
 "use strict";
-let number = 20;
-const leftBorderWidth = 1;
-number = 10;
-console.log(number);
+const numberofFilms = +prompt('Сколько фильмов вы посмотрели?',"");
+const personalMovieDB = {
+count: numberofFilms,
+movies: {},
+actors: {},
+genres:[],
+privat:false,
+};
 
-const storeName ='gucci';
-const storeDescription = {
-    budget:10000,
-    employes:[
-        'dima','anna','katya'
-        ],
-         products:{
-            potatoe:20,
-            sprite:10
-             },
-               open:true
-}
-console.log(storeDescription);
+const a = prompt('Один из последних просмотреных фильмов?',''),
+      b =  prompt('Насколько оцените его?',''),
+      c = prompt('Один из последних просмотреных фильмов?',''),
+      d = prompt('Насколько оцените его?','');
 
-
-// const answers = []
-// answers[0] = prompt('Write your name?', '');
-// answers[1] = prompt('Write your surename?', '');
-// answers[2] = prompt('Write your age?', '');
-// document.write(answers);
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-    
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+      console.log(personalMovieDB);
